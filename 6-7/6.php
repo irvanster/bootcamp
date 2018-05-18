5af28c2c94946cdf26111d229a454254136d2e3f
Tofa Maulana Irvan <tovamaulana098@gmail.com>
2018-05-18 22:13:13 +0700

    Update 6.php

--- 6-7/6.php
+++ 6-7/6.php
@@ -2,6 +2,6 @@
 
 
 //posts
-SELECT * FROM posts join users ON users.id_user = posts.createdBy; <br>
-//comments <br>
+SELECT * FROM posts join users ON users.id_user = posts.createdBy; 
+//comments 
 SELECT * FROM comments WHERE postId=$id;
